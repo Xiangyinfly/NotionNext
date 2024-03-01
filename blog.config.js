12d9d70d656fa37d5ad50c7bde122b689870df74
沙丁鱼 <81477590+Xiangyinfly@users.noreply.github.com>
2024-03-01 21:31:17 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -13,7 +13,7 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
-  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || '希望你可以记住我,记住我这样活过,这样在你身边呆过,欢迎来到沙丁鱼的小窝ヾ(*´▽'*)ﾉ',
+  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || '希望你可以记住我,记住我这样活过,这样在你身边呆过,欢迎来到沙丁鱼的小窝～',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
